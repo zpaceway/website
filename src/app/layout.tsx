b6e35e7ec68a7b56254b747729d5a5b08429647d
Alexandro Tapia <zpaceway@gmail.com>
2023-01-16 21:56:36 -0500

    add missing breakpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="text-[14px] xs:text-[16px] md:text-[18px] xl[20px]"
+      className="text-[14px] xs:text-[16px] md:text-[18px] xl:text-[20px]"
     >
       <head />
       <body>{children}</body>
