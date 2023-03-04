0bf03dd35d4ca555362c62d15b49e66492b3ee57
Alexandro Tapia <zpaceway@gmail.com>
2023-03-04 13:20:33 -0500

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import "./globals.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -11,8 +13,29 @@ export default function RootLayout({
       lang="en"
       className="text-[14px] xs:text-[16px] md:text-[18px] xl:text-[20px]"
     >
+      <Script
+        id="gtag-header"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NJZTHVT');`,
+        }}
+      ></Script>
       <head />
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJZTHVT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
