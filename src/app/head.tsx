9f0bbb2840b19a67ead7529cf8efb5126118b07f
Alexandro Tapia <zpaceway@gmail.com>
2023-03-09 00:57:01 -0500

    use ganalytics instead of gtag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,13 +4,18 @@ export default function Head() {
   return (
     <>
       <Script
-        id="gtag-header"
+        id="gtag-async-script"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-L5GLGTEGF4"
+      ></Script>
+
+      <Script
+        id="gtag-sync-script"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NJZTHVT');`,
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-L5GLGTEGF4');`,
         }}
       ></Script>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import "./globals.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -13,17 +12,7 @@ export default function RootLayout({
       className="text-[14px] xs:text-[16px] md:text-[18px] xl:text-[20px]"
     >
       <head />
-      <body>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NJZTHVT"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
